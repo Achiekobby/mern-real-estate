454b59f79d8d430b76d5632803ab6a20dd560342
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-06 05:45:38 +0000

    database connection established

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
-
 const app = express();
 
-const port = process.env.PORT_NUMBER;
-app.listen(port,()=>{
-  console.log(`Server is listening on port: ${port}`);
-});
+//* method to start server
+function start_server(port_number) {
+  const port = process.env.PORT_NUMBER;
+  app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+  });
+}
+
+//* database connection
+const mongo_url = process.env.DB_CONNECTION_URL;
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("Database connection established");
+    start_server();
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
