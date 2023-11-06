3ba783e835358debead4d61663bc5330de9eb4a0
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-06 07:22:36 +0000

    appending necessary middleware packages to the backend

---
+++ backend/app.js
@@ -0,0 +1,63 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
+class App {
+  port;
+  mongo_uri;
+  express;
+
+  constructor(controllers, mongo_uri) {
+    this.express = express();
+    dotenv.config();
+    this.port = process.env.PORT_NUMBER;
+    this.mongo_uri = mongo_uri;
+
+    //* custom methods
+    this.initiateMiddleware();
+    this.establishDBConnection();
+    this.initiateControllers(controllers);
+  }
+
+  initiateMiddleware() {
+    this.express.use(cors());
+    this.express.use(helmet());
+    this.express.use(compression());
+    this.express.use(morgan("dev"));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+  }
+
+  establishDBConnection() {
+    mongoose
+      .connect(this.mongo_uri)
+      .then(() => {
+        console.log("DB connection established");
+      })
+      .catch((err) => {
+        console.log(`Error: ${err.message}`);
+      });
+  }
+
+  initiateControllers(controllers) {
+    try {
+      controllers.forEach((controller) => {
+        this.express.use("/api", controller.router);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  listen() {
+    this.express.listen(this.port, () => {
+      console.log(`Server is connected on port: ${this.port}`);
+    });
+  }
+}
+
+export default App;
--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,8 @@
-import express from "express";
+//* import the app
+import App from "./app.js";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 
 dotenv.config();
-const app = express();
-
-//* method to start server
-function start_server() {
-  const port = process.env.PORT_NUMBER;
-  app.listen(port, () => {
-    console.log(`Server is listening on port: ${port}`);
-  });
-}
-
-//* database connection
-const mongo_url = process.env.DB_CONNECTION_URL;
-mongoose
-  .connect(mongo_url)
-  .then(() => {
-    console.log("Database connection established");
-    start_server();
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const db_uri = process.env.DB_CONNECTION_URL;
+const app = new App([], db_uri);
+app.listen();
