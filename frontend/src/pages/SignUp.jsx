f17fa189e16afc2cef49305610d37734256b69c4
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-10 11:37:01 +0000

    building the payload for the sign-up

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,39 +4,67 @@ import "react-international-phone/style.css";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  const [phone, setPhone] = useState("");
+  const [form_data, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    phone_number: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    console.log(form_data);
+  };
+
+  const handlePhoneNumberChange = (phone) => {
+    const cleanedPhone = phone.startsWith('+2330') ? `+233${phone.slice(5)}` : phone;
+    setFormData((prevFormData) => ({ ...prevFormData, phone_number: cleanedPhone }));
+  };
+
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    console.log(form_data);
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
+          value={form_data.first_name}
           type="text"
           placeholder="First Name"
           className="border p-3 rounded-lg"
-          id="first_name"
+          name="first_name"
         />
         <input
+          onChange={handleChange}
+          value={form_data.last_name}
           type="text"
           placeholder="Last Name"
           className="border p-3 rounded-lg"
           id="last_name"
+          name="last_name"
         />
         <input
+          onChange={handleChange}
+          value={form_data.email}
           type="email"
           placeholder="Email"
           className="border p-3 rounded-lg"
-          id="email"
+          name="email"
         />
 
         <div className="border p-3 rounded-lg bg-white">
           <PhoneInput
             inputClassName="react-international-phone-height"
-            className=""
             defaultCountry="gh"
-            value={phone}
-            onChange={(phone) => {
-              setPhone(phone);
-            }}
+            name="phone"
+            onChange={handlePhoneNumberChange}
+            value={form_data.phone}
             style={{
               "--react-international-phone-border-radius": "10px",
               "--react-international-phone-border-color": "#fff",
@@ -50,7 +78,9 @@ export default function SignUp() {
           type="password"
           placeholder="Password***"
           className="border p-3 rounded-lg"
-          id="password"
+          name="password"
+          onChange={handleChange}
+          value={form_data.password}
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           Sign Up
