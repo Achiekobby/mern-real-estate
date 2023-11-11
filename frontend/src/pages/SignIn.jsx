3ceef1b20863cda8879c823d190ab7b3ce69f323
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-11 18:53:06 +0000

    complete login implementation frontend

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,6 +1,133 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {BiError} from "react-icons/bi"
 
 export default function SignIn() {
+  const [form_data, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [validationError, setValidationError] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    console.log(form_data);
+  };
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form_data),
+      });
+      const data = await response.json();
+      if (data.status === "failed") {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      if (data.errors && data.errors.length > 0) {
+        setLoading(false);
+        data.errors.forEach((error) => {
+          const match = error.match(/"([^"]*)"/);
+          const error_message = match && match[1];
+          switch (error_message) {
+            case "email":
+              setValidationError((prevValidationError) => ({
+                ...prevValidationError,
+                email: error.replace(/"([^"]*)"/, '$1'),
+              }));
+              break;
+            case "password":
+              setValidationError((prevValidationError) => ({
+                ...prevValidationError,
+                password: error.replace(/"([^"]*)"/, '$1'),
+              }));
+              break;
+            default:
+              break;
+          }
+        });
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+      console.log("error:", error);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      {error && <p className="capitalize text-xs text-red-700">{error}</p>}
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={form_data.email}
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          name="email"
+        />
+        {validationError.email && (
+          <>
+          <p className="flex items-center capitalize text-xs text-red-400">
+          <span>
+            <BiError className="text-red-600 text-sm mr-2" />
+          </span>
+          {validationError.email}
+          </p>
+          </>
+        )}
+
+        <input
+          type="password"
+          placeholder="Password***"
+          className="border p-3 rounded-lg"
+          name="password"
+          onChange={handleChange}
+          value={form_data.password}
+        />
+        {validationError.password && (
+          <>
+          <p className="flex items-center capitalize text-xs text-red-400">
+          <span>
+            <BiError className="text-red-600 text-sm mr-2" />
+          </span>
+          {validationError.password}
+          </p>
+          </>
+        )}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading" : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 text-center">
+        <p className="text-slate-800">{"Don't have an Account?"} </p>
+        <Link to="/sign-in">
+          <span className="text-green-700">Sign-In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
