b37e0996ad53f2d4b50f2a7fec64ed6d08f02925
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-14 07:50:56 +0000

    update the header and making the profile page private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Profile from './pages/Profile';
+import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about-us" element={<About/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+{/* 
+        //* Declaring a private Route */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const { currentUser } = useSelector(state=>state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,10 +32,15 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
-              Sign-in
-            </li>
+          <Link to="/profile">
+            {
+              currentUser ? (
+                <img className="rounded-full h-7 w-7 object-cover" src={currentUser.user_info.avatar} alt="https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg" />
+              ):
+              (
+                <li className="text-slate-700 hover:underline">Sign-In</li>
+              )
+            }
           </Link>
         </ul>
       </div>
