8cf58d66b4eee66cc8a0c9b5265e01fb8e0292a7
Kwabena Nana Acheampong <achiekobby17@gmail.com>
2023-11-24 07:26:34 +0000

    image upload feature completed

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,4 +1,77 @@
+import { useState } from "react";
+import { app } from "../firebase";
+import { MdOutlineSmsFailed } from "react-icons/md";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+
 export default function CreateListing() {
+  //* states
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({ image_urls: [] });
+  const [imageUploadError, setImageUploadError] = useState(false);
+
+  console.log("formData", formData);
+
+  //* function to handle the image upload
+  const handleImageUpload = () => {
+    if (
+      files.length > 0 &&
+      files.length < 7 &&
+      formData.image_urls.length < 7
+    ) {
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        promises.push(storeImage(file));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            image_urls: formData.image_urls.concat(urls),
+          });
+          setImageUploadError(false);
+        })
+        .catch((err) => {
+          console.log("error:", err);
+          setImageUploadError("Image upload failed (2MB max per image)");
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+    }
+  };
+
+  //* an async function to store image in firebase
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% complete`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -109,14 +182,45 @@ export default function CreateListing() {
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
-          <p className="font-semibold">Images:
-          <span className="font-normal text-gray-600 ml-2">The first image will be the cover (max:6)</span>
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max:6)
+            </span>
           </p>
           <div className="flex gap-4">
-            <input type="file" id="images" accept="images/*"  multiple className="p-3 border border-gray-300 w-full"/>
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+            <input
+              type="file"
+              id="images"
+              accept="images/*"
+              multiple
+              className="p-3 border border-gray-300 w-full"
+              onChange={(event) => setFiles(event.target.files)}
+            />
+            <button
+              onClick={handleImageUpload}
+              type="button"
+              className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
+              Upload
+            </button>
           </div>
-        <button className="p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">Create a Listing</button>
+          <p className="text-red-700 text-xs font-bold flex gap-2 items-center">
+            {imageUploadError && (
+              <>
+                <span className="text-sm font-bold">
+                  <MdOutlineSmsFailed />
+                </span>
+                {imageUploadError}
+              </>
+            )}
+          </p>
+
+          {/* preview the images to be uploaded */}
+          
+          <button className="p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
+            Create a Listing
+          </button>
         </div>
       </form>
     </main>
